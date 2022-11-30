55182dfc70e584ad0a2b79f4d0adb5b88a1b0d57
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-11-30 14:23:17 -0300

    Initial commit

---
+++ src/test/java/com/samuca/bank/credito/MessageGeneratorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.samuca.bank.credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		System.out.println("Ola Mundo!!!");
+	}
+
+}
